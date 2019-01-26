5b1f9dff4db855efaf6acd7efb35a39542c2d41a
gugutz <gugutz@gmail.com>
2019-01-26 20:53:05 -0200

    task: complementing previous commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,41 +8,43 @@ import '../css/bulma.min.css'
 import '../css/lpitz.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
+//   <StaticQuery
+//     query={graphql`
+//       query SiteTitleQuery {
+//         site {
+//           siteMetadata {
+//             title
+//           }
+//         }
+//       }
+//     `}
     render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © {new Date().getFullYear()}, made by 
-            {` `}
-            <a href="https://github.com/gugutz">gustavo p. borges</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+        <p></p>
+//       <>
+//         <Header siteTitle={data.site.siteMetadata.title} />
+//         <div
+//           style={{
+//             margin: `0 auto`,
+//             maxWidth: 960,
+//             padding: `0px 1.0875rem 1.45rem`,
+//             paddingTop: 0,
+//           }}
+//         >
+//           {children}
+//           <footer>
+//             © {new Date().getFullYear()}, made by 
+//             {` `}
+//             <a href="https://github.com/gugutz">gustavo p. borges</a>
+//           </footer>
+//         </div>
+//       </>
+//     )}
+//   />
 )
 
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
-}
+// Layout.propTypes = {
+//   children: PropTypes.node.isRequired,
+// }
 
 export default Layout
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import { Link } from 'gatsby'
+// import React from 'react'
+// import { Link } from 'gatsby'
 
-import Layout from '../components/layout'
-import Image from '../components/image'
-import SEO from '../components/seo'
+// import Layout from '../components/layout'
+// import Image from '../components/image'
+// import SEO from '../components/seo'
 
-const IndexPage = () => (
-  <Layout>
-    <p></p>
-  </Layout>
-)
+// const IndexPage = () => (
+  // <Layout>
+  //   <p></p>
+  // </Layout>
+// )
 
-export default IndexPage
+// export default IndexPage
+
